d7be3ad0b33bc66cc511ba778f1efe0cd86a4289
Nithilan Rameshkumar <dev@nithitsuki.com>
2026-06-22 16:02:37 +0530

    new screenshot tool

--- emacs/.config/doom/packages.el
+++ emacs/.config/doom/packages.el
@@ -22,8 +22,6 @@
 ;; https://github.com/radian-software/straight.el#the-recipe-format
 ;; (package! another-package
 ;;   :recipe (:host github :repo "username/repo"))
-(package! timetabel
-  :recipe (:host github :repo "nairadithya/timetab.el"))
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
 ;; `:files' in the `:recipe':
